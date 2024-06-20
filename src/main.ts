98a15c86006abfbc530f529d3d4fc66b32c214f0
Kevin Ricardo Palencia Gil <kevingil1910@gmail.com>
2024-06-20 11:22:45 -0600

    feat: fetch originazation pkg versions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -7,18 +6,25 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const packageUrl: string = core.getInput('organization-package-url')
+    const gh_token: string = core.getInput('github-token')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Getting the organization package at ${packageUrl}`)
+    let response = await fetch(packageUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${gh_token}`
+      }
+    }).then(response => {
+      return response.json()
+    })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let versions: string[] = response.data.map((pkg: any) => pkg.name)
+    core.debug(`Found versions: ${versions}`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('versions', versions)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
