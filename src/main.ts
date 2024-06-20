f182e3dd496beb40bc4a677ceab40df5ad4f0780
Kevin Ricardo Palencia Gil <kevingil1910@gmail.com>
2024-06-20 12:45:30 -0600

    feat: fetch delete api to remove last package version order by date

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
 import { PackageVersion } from './package_version'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
     const packageUrl: string = core.getInput('organization-package-url')
     const gh_token: string = core.getInput('gh-token')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Getting the organization package at ${packageUrl}`)
     const response = await fetch(packageUrl, {
       method: 'GET',
@@ -27,11 +22,36 @@ export async function run(): Promise<void> {
     const versions: string[] = res.map((version: PackageVersion) => {
       return version.name
     })
+    core.info(`Found ${versions.length} versions`)
 
-    // Set outputs for other workflow steps to use
+    // order versions by date
+
+    const sortedVersions: PackageVersion[] = [...res].sort((a, b) => {
+      const dateA = new Date(a.created_at)
+      const dateB = new Date(b.created_at)
+      return dateA > dateB ? 1 : -1
+    })
+
+    // get the older version
+    const olderVersion = sortedVersions[0]
+    core.info(`Deleting version ${olderVersion.name} ... `)
+
+    //remove with github api the older version
+    const deleteResponse = await fetch(`${packageUrl}/${olderVersion.id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${gh_token}`
+      }
+    })
+
+    if (!deleteResponse.ok) {
+      throw new Error(`Failed to delete package: ${deleteResponse.statusText}`)
+    }
+
+    core.info(`Deleted version ${olderVersion.name}`)
+
     core.setOutput('versions', versions)
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
